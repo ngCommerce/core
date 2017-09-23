73fa0f042e503a5213fe364adb9f7b931a822bb8
jigkoh3 <jigkoh3@hotmail.com>
2017-09-24 05:59:38 +0700

    add main .ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright Cyber Advance System and Network CO.,LTD. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ */
+
+export * from './src/index';
