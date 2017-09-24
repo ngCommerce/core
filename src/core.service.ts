19e6e6ceb1eed15296a46a55143886b1aade69e5
jigkoh3 <jigkoh3@hotmail.com>
2017-09-24 07:35:33 +0700

    It's cool of worked.

--- src/core.service.ts
+++ src/core.service.ts
@@ -7,13 +7,10 @@ export const API_URL = "";
 export class CorService {
 
     get apiURL(): String {
-        //console.log('object');
         return this._apiURL;
-        //return "test";
     }
     private _apiURL: String;
     constructor(@Inject(API_URL) apiURL: String) {
-        //console.log(apiURL);
         this._apiURL = apiURL;
     }
     
