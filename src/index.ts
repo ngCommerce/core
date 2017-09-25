e22fce13094b04281039502d844d9ec0f9f6b9fb
jigkoh3 <jigkoh3@hotmail.com>
2017-09-25 18:45:50 +0700

    product and shop

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { EcommerceCoreModule } from "./core.module";
 export { CorService, API_URL } from "./core.service";
 export { CategoryService } from "./category/category.service";
+export { ProductService } from "./product/product.service";
+export { ShopService } from "./shop/shop.service";
