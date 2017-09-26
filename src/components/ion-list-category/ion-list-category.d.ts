7b92bebb2879130508fad81e2ac76b8247d80e7e
jigkoh3 <jigkoh3@hotmail.com>
2017-09-26 10:17:29 +0700

    ttt

---
+++ src/components/ion-list-category/ion-list-category.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Generated class for the IonListCategoryComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+export declare class IonListCategoryComponent {
+    items: any;
+    constructor();
+}
--- src/core.module.ts
+++ src/core.module.ts
@@ -7,12 +7,7 @@ import { CategoryService } from "./services/category/category.service";
 import { ShopService } from "./services/shop/shop.service";
 import { ProductService } from './services/product/product.service';
 
-import { IonItemCategoryComponent } from "./components/ion-item-category/ion-item-category";
-import { IonItemProductComponent } from "./components/ion-item-product/ion-item-product";
-import { IonItemShopComponent } from "./components/ion-item-shop/ion-item-shop";
 import { IonListCategoryComponent } from "./components/ion-list-category/ion-list-category";
-import { IonListProductComponent } from "./components/ion-list-product/ion-list-product";
-import { IonListShopComponent } from "./components/ion-list-shop/ion-list-shop";
 
 @NgModule()
 export class EcommerceCoreModule {
