656b477882cb06b052e8b70f2060810e970f150d
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-09-29 14:48:46 +0700

    update model

--- src/models/cart.model.ts
+++ src/models/cart.model.ts
@@ -3,22 +3,16 @@ import { ProductModel } from "./product.model";
 export interface CartModel {
     _id: string;
     items: Array<CartItemModel>;
-    totalPrice: number;
+    amount: number;
+    discount: number;
+    totalamount: number;
 }
 
 export interface CartItemModel {
     _id: string;
     product: ProductModel;
-    delivery: DeliveryModel;
     qty: number;
     amount: number;
     discount: number;
     totalamount: number;
-    deliveryprice: number;
-}
-
-export interface DeliveryModel {
-    detail: string;
-    name: string;
-    price: number;
 }
