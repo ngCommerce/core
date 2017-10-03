e13830ffa7a7caa6bb92eee89f3cd94d67fee7ed
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-03 15:38:54 +0700

    update

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -99,6 +99,31 @@ export class CartService {
         return window.localStorage.getItem('gCart') ? JSON.parse(window.localStorage.getItem('gCart')) : null;
     }
 
+    saveCartStorage(cart) {
+        window.localStorage.setItem('gCart', JSON.stringify(cart));
+        return;
+    }
+
+    onCalculate(cart) {
+        cart.amount = 0;
+        cart.discount = 0;
+        cart.totalamount = 0;
+
+        for (var i = 0; i < cart.items.length; i++) {
+            let item = cart.items[i];
+            let promotionprice = item.product.promotionprice ? item.product.promotionprice : 0;
+            item.amount = item.product.price * item.qty;
+            item.discount = promotionprice > 0 ? (item.product.price - promotionprice) * item.qty : 0;
+            item.totalamount = item.amount - item.discount;
+
+            cart.amount += item.amount;
+            cart.discount += item.discount;
+            cart.totalamount += item.totalamount;
+        }
+
+        return cart;
+
+    }
 
     private handleError(error: any): Promise<any> {
         return Promise.reject(error.message || error);
