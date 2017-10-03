f25a5ff3f697660c3026fca05751847f3e55d5d9
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-03 15:32:16 +0700

    update

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -61,6 +61,45 @@ export class CartService {
     }
 
 
+    addToCart(product) {
+        let cartStorage = window.localStorage.getItem('gCart') ? JSON.parse(window.localStorage.getItem('gCart')) : null;
+        if (cartStorage && cartStorage.items.length > 0) {
+
+            for (var i = 0; i < cartStorage.items.length; i++) {
+                var item = cartStorage.items[i];
+                if (product._id === item.product._id) {
+                    item.qty++;
+                    item.amount = product.price * item.qty;
+                    item.discount = product.promotionprice ? (product.price - product.promotionprice) * item.qty : 0;
+                    item.totalamount = item.amount - item.discount;
+                    window.localStorage.setItem('gCart', JSON.stringify(cartStorage));
+                    this.cartStorage();
+                }
+            }
+
+        }
+        if (!cartStorage) {
+            cartStorage = {
+                items: []
+            }
+        }
+        cartStorage.items.push({
+            product: product,
+            qty: 1,
+            amount: product.price,
+            discount: product.promotionprice ? (product.price - product.promotionprice) : 0,
+            totalamount: product.promotionprice
+        });
+        window.localStorage.setItem('gCart', JSON.stringify(cartStorage));
+        this.cartStorage();
+
+    }
+
+    cartStorage() {
+        return window.localStorage.getItem('gCart') ? JSON.parse(window.localStorage.getItem('gCart')) : null;
+    }
+
+
     private handleError(error: any): Promise<any> {
         return Promise.reject(error.message || error);
     }
