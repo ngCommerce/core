7bb7687f04553eed26c915ea0836f102baa9d6e1
mob35 <toonelicious@gmail.com>
2017-10-03 14:04:21 +0700

    payment

--- src/components/ion-form-confirm/ion-form-confirm.js
+++ src/components/ion-form-confirm/ion-form-confirm.js
@@ -18,36 +18,123 @@ const core_1 = require("@angular/core");
  */
 let IonFormConfirmComponent = class IonFormConfirmComponent {
     constructor() {
-        this.itemClicked = new core_1.EventEmitter();
+        this.gotoNext = new core_1.EventEmitter();
         // console.log('Hello IonFormConfirmComponent Component');
     }
-    onClick(item) {
-        this.itemClicked.emit(item);
+    discount(data) {
+        if (data && data > 0) {
+            if (this.confirmgateway.order.amount && this.confirmgateway.order.amount >= data) {
+                this.confirmgateway.order.discount = data;
+            }
+            else {
+                this.confirmgateway.order.discount = this.confirmgateway.order.amount;
+            }
+        }
+        else {
+            this.confirmgateway.order.discount = 0;
+        }
+    }
+    stepValidation() {
+        if (this.confirmgateway.order.discount && this.confirmgateway.order.discount > 0 && !undefined) {
+            this.confirmgateway.order.totalamount = ((this.confirmgateway.order.tran || 0) + (this.confirmgateway.order.amount || 0)) - (this.confirmgateway.order.discount || 0);
+        }
+        else {
+            this.confirmgateway.order.totalamount = ((this.confirmgateway.order.tran || 0) + (this.confirmgateway.order.amount || 0));
+        }
+        this.gotoNext.emit(this.confirmgateway);
     }
 };
 __decorate([
     core_1.Input(),
     __metadata("design:type", Object)
-], IonFormConfirmComponent.prototype, "items", void 0);
+], IonFormConfirmComponent.prototype, "confirmgateway", void 0);
 __decorate([
     core_1.Output(),
     __metadata("design:type", core_1.EventEmitter)
-], IonFormConfirmComponent.prototype, "itemClicked", void 0);
+], IonFormConfirmComponent.prototype, "gotoNext", void 0);
 IonFormConfirmComponent = __decorate([
     core_1.Component({
         selector: 'ion-form-confirm',
         template: `
-    <ion-list>
-    
-      <ion-item>
-        
-      </ion-item>
-    
-    </ion-list>
-    
-    <div>
-      <button ion-button block (click)="onClick(item)">Submit</button>
-    </div>
+    <ion-content *ngIf="confirmgateway.order">
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-list no-lines>
+            <ion-item>
+              <p>มูลค่าสินค้า</p>
+              <p item-end>{{confirmgateway.order.amount | number}} บาท</p>
+            </ion-item>
+            <ion-item>
+              <p>ค่าธรรมเนียมการจัดส่งสินค้า</p>
+              <p item-end>{{confirmgateway.order.tran | number}} บาท</p>
+            </ion-item>
+            <ion-row>
+              <ion-col col-8>
+                <ion-item>
+                  <ion-input type="text" [(ngModel)]="confirmgateway.order.discount" (ngModelChange)="discount(confirmgateway.order.discount)" placeholder="ใส่โค้ดส่วนลดตรงนี้"></ion-input>
+                </ion-item>
+              </ion-col>
+              <ion-col col-4>
+                <button ion-button block color="warning" (click)="discount(confirmgateway.order.discount)">ยืนยัน</button>
+              </ion-col>
+            </ion-row>
+            <ion-item>
+              <p><b>ยอดสุทธิ (รวมภาษีมูลค่าเพิ่ม)</b></p>
+              <p item-end><b>{{((confirmgateway.order.tran || 0) + (confirmgateway.order.amount || 0)) - (confirmgateway.order.discount || 0) | number}} บาท</b></p>
+            </ion-item>
+          </ion-list>
+          <ion-item-group>
+            <ion-item-divider color="light"><b>รายการ</b></ion-item-divider>
+          </ion-item-group>
+          <div>
+            <ion-list *ngFor="let item of confirmgateway.order.items">
+              <ion-item>
+                <ion-row>
+                  <ion-col col-12>
+                    <p>{{item.product.shop.name}}</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row no-padding>
+                  <ion-col no-padding width-20>
+                    <ion-preload-image [ratio]="{w:1, h:1}" src="{{item.product.image}}"></ion-preload-image>
+                  </ion-col>
+                  <ion-col no-padding width-60>
+                    <h3>{{item.product.name}}</h3>
+                    <p>จำนวน: {{item.qty}}</p>
+                  </ion-col>
+                  <ion-col no-padding width-20>
+                    <p>{{item.product.price}} บาท</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row no-padding>
+                  <ion-col padding width-100 text-center>
+                    <p>{{item.delivery.name}}</p>
+                    <p>{{item.delivery.detail}}</p>
+                  </ion-col>
+                </ion-row>
+              </ion-item>
+            </ion-list>
+            <ion-row>
+              <p>ที่อยู่จัดส่ง</p>
+              <ion-col col-12>
+                <p>{{confirmgateway.order.shipping.firstname}} {{confirmgateway.order.shipping.lastname}}</p>
+                <p>{{confirmgateway.order.shipping.address}} {{confirmgateway.order.shipping.subdistrict}} {{confirmgateway.order.shipping.district}}
+                  {{confirmgateway.order.shipping.province}} {{confirmgateway.order.shipping.postcode}}
+                </p>
+                <p>โทร : {{confirmgateway.order.shipping.tel}}</p>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col col-12>
+                <button ion-button block color="warning" (click)="stepValidation()">สั่งสินค้า</button>
+              </ion-col>
+            </ion-row>
+          </div>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-content>
     `,
         styles: [`
   ion-form-confirm {
--- src/components/ion-form-confirm/ion-form-confirm.ts
+++ src/components/ion-form-confirm/ion-form-confirm.ts
@@ -9,17 +9,85 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
 @Component({
     selector: 'ion-form-confirm',
     template: `
-    <ion-list>
-    
-      <ion-item>
-        
-      </ion-item>
-    
-    </ion-list>
-    
-    <div>
-      <button ion-button block (click)="onClick(item)">Submit</button>
-    </div>
+    <ion-content *ngIf="confirmgateway.order">
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-list no-lines>
+            <ion-item>
+              <p>มูลค่าสินค้า</p>
+              <p item-end>{{confirmgateway.order.amount | number}} บาท</p>
+            </ion-item>
+            <ion-item>
+              <p>ค่าธรรมเนียมการจัดส่งสินค้า</p>
+              <p item-end>{{confirmgateway.order.tran | number}} บาท</p>
+            </ion-item>
+            <ion-row>
+              <ion-col col-8>
+                <ion-item>
+                  <ion-input type="text" [(ngModel)]="confirmgateway.order.discount" (ngModelChange)="discount(confirmgateway.order.discount)" placeholder="ใส่โค้ดส่วนลดตรงนี้"></ion-input>
+                </ion-item>
+              </ion-col>
+              <ion-col col-4>
+                <button ion-button block color="warning" (click)="discount(confirmgateway.order.discount)">ยืนยัน</button>
+              </ion-col>
+            </ion-row>
+            <ion-item>
+              <p><b>ยอดสุทธิ (รวมภาษีมูลค่าเพิ่ม)</b></p>
+              <p item-end><b>{{((confirmgateway.order.tran || 0) + (confirmgateway.order.amount || 0)) - (confirmgateway.order.discount || 0) | number}} บาท</b></p>
+            </ion-item>
+          </ion-list>
+          <ion-item-group>
+            <ion-item-divider color="light"><b>รายการ</b></ion-item-divider>
+          </ion-item-group>
+          <div>
+            <ion-list *ngFor="let item of confirmgateway.order.items">
+              <ion-item>
+                <ion-row>
+                  <ion-col col-12>
+                    <p>{{item.product.shop.name}}</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row no-padding>
+                  <ion-col no-padding width-20>
+                    <ion-preload-image [ratio]="{w:1, h:1}" src="{{item.product.image}}"></ion-preload-image>
+                  </ion-col>
+                  <ion-col no-padding width-60>
+                    <h3>{{item.product.name}}</h3>
+                    <p>จำนวน: {{item.qty}}</p>
+                  </ion-col>
+                  <ion-col no-padding width-20>
+                    <p>{{item.product.price}} บาท</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row no-padding>
+                  <ion-col padding width-100 text-center>
+                    <p>{{item.delivery.name}}</p>
+                    <p>{{item.delivery.detail}}</p>
+                  </ion-col>
+                </ion-row>
+              </ion-item>
+            </ion-list>
+            <ion-row>
+              <p>ที่อยู่จัดส่ง</p>
+              <ion-col col-12>
+                <p>{{confirmgateway.order.shipping.firstname}} {{confirmgateway.order.shipping.lastname}}</p>
+                <p>{{confirmgateway.order.shipping.address}} {{confirmgateway.order.shipping.subdistrict}} {{confirmgateway.order.shipping.district}}
+                  {{confirmgateway.order.shipping.province}} {{confirmgateway.order.shipping.postcode}}
+                </p>
+                <p>โทร : {{confirmgateway.order.shipping.tel}}</p>
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col col-12>
+                <button ion-button block color="warning" (click)="stepValidation()">สั่งสินค้า</button>
+              </ion-col>
+            </ion-row>
+          </div>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-content>
     `,
     styles: [`
   ion-form-confirm {
@@ -28,12 +96,32 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
     ]
 })
 export class IonFormConfirmComponent {
-    @Input() items: any;
-    @Output() itemClicked: EventEmitter<any> = new EventEmitter<any>();
+    data: any;
+    @Input() confirmgateway: any;
+    @Output() gotoNext: EventEmitter<any> = new EventEmitter<any>();
+    confirmdiscount: number;
     constructor() {
         // console.log('Hello IonFormConfirmComponent Component');
     }
-    onClick(item) {
-        this.itemClicked.emit(item);
+    discount(data) {
+        if (data && data > 0) {
+            if (this.confirmgateway.order.amount && this.confirmgateway.order.amount >= data) {
+                this.confirmgateway.order.discount = data;
+            } else {
+                this.confirmgateway.order.discount = this.confirmgateway.order.amount;
+            }
+        } else {
+            this.confirmgateway.order.discount = 0;
+        }
     }
+    stepValidation() {
+
+        if (this.confirmgateway.order.discount && this.confirmgateway.order.discount > 0 && !undefined) {
+            this.confirmgateway.order.totalamount = ((this.confirmgateway.order.tran || 0) + (this.confirmgateway.order.amount || 0)) - (this.confirmgateway.order.discount || 0);
+        } else {
+            this.confirmgateway.order.totalamount = ((this.confirmgateway.order.tran || 0) + (this.confirmgateway.order.amount || 0));
+        }
+        this.gotoNext.emit(this.confirmgateway);
+    }
+
 }
