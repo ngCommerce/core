56997e0ef82db05dff16e12162a9fd9f0a62c88c
moomam6934 <destinationpainbm@gmail.com>
2017-10-04 11:23:58 +0700

    review

--- src/components/ion-detail-product/ion-detail-product.js
+++ src/components/ion-detail-product/ion-detail-product.js
@@ -83,6 +83,70 @@ IonDetailProductComponent.decorators = [
         </ion-col>
     </ion-row>
 </ion-grid>
+<ion-grid *ngIf="item">
+<ion-row>
+    <ion-col width-50>
+        <ion-row>
+            <ion-col>
+                <rating [(ngModel)]="rate" readOnly="false" max="5" emptyStarIconName="star-outline" halfStarIconName="star-half"
+                    starIconName="star" nullable="false"></rating>
+            </ion-col>
+        </ion-row>
+        <ion-row>
+            <ion-col>
+                <span> {{item.rate}} </span> <span>From 5</span>
+            </ion-col>
+        </ion-row>
+        <ion-row>
+            <ion-col>
+                <span>{{item ? item.length : 0}} รีวิว</span>
+            </ion-col>
+        </ion-row>
+    </ion-col>
+    <ion-col width-50>
+        <p *ngFor="let group of groups">
+            <ion-row>
+                <ion-col width-30>
+                    <span>{{group.name}}STAR</span>
+                </ion-col>
+                <ion-col width-60>
+                    <span [style.width]="group.percent"></span>
+                </ion-col>
+                <ion-col width-10>
+                    <span>{{group.sum}}</span>
+                </ion-col>
+            </ion-row>
+        </p>
+    </ion-col>
+</ion-row>
+<ion-row>
+    <ion-col>
+        <button ion-button block outline>Write Review</button>
+    </ion-col>
+</ion-row>
+</ion-grid>
+<hr *ngIf="items">
+<ion-list *ngIf="items">
+<ion-item-divider *ngFor="let review of items">
+    <p>
+        <ion-row>
+            <ion-col no-padding width-60 text-left>
+                <p>{{review.topic}}</p>
+            </ion-col>
+            <ion-col width-40 text-right>
+                <p>{{review.created | moment: 'LL'}}</p>
+                <p>{{review.user ? review.user.displayname : ''}}</p>
+            </ion-col>
+        </ion-row>
+    </p>
+    <p>
+        <rating [(ngModel)]="review.rate" readOnly="false" max="5" emptyStarIconName="star-outline" halfStarIconName="star-half"
+            starIconName="star" nullable="false"></rating>
+    </p>
+    <p>{{review.comment}}</p>
+    <p></p>
+</ion-item-divider>
+</ion-list>
     `,
                 styles: [`
   ion-detail-product {
