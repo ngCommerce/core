0a33b7ab5c23155e5537e5da3bc40974f9e844ba
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-05 11:48:32 +0700

    1.1.66

--- src/components/ion-list-grid/ion-list-grid.js
+++ src/components/ion-list-grid/ion-list-grid.js
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 /**
  * Generated class for the IonListCategoryComponent component.
  *
@@ -12,48 +12,33 @@ export class IonListGridComponent {
 }
 IonListGridComponent.decorators = [
     { type: Component, args: [{
-                selector: "ion-list-grid",
+                selector: 'ion-list-grid',
                 template: `
     <ion-grid>
     <ion-row>
       <ion-col col-6 *ngIf="items && items.length > 0">
         <img src="{{items[0].image}}">
-        <div class="p-margin-top-50">
-        <p>{{items[0].name}}</p>
-        <p>{{items[0].price}}</p>
-        </div>
+        <p class="p-margin-top-50">{{items[0].name}} {{items[0].price}}</p>
       </ion-col>
       <ion-col col-6>
         <ion-row>
           <ion-col col-6 *ngIf="items && items.length > 1">
             <img src="{{items[1].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[1].name}}</p>
-            <p>{{items[1].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[1].name}} {{items[1].price}}</p>
           </ion-col>
           <ion-col col-6 *ngIf="items && items.length > 2">
             <img src="{{items[2].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[2].name}}</p>
-            <p>{{items[2].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[2].name}} {{items[2].price}}</p>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col col-6 *ngIf="items && items.length > 3">
             <img src="{{items[3].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[3].name}}</p>
-            <p>{{items[3].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[3].name}} {{items[3].price}}</p>
           </ion-col>
           <ion-col col-6 *ngIf="items && items.length > 4">
             <img src="{{items[4].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[4].name}}</p>
-            <p>{{items[4].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[4].name}} {{items[4].price}}</p>
           </ion-col>
         </ion-row>
       </ion-col>
@@ -61,8 +46,7 @@ IonListGridComponent.decorators = [
   </ion-grid>
   
     `,
-                styles: [
-                    `
+                styles: [`
     .p-margin-top-50 {
       margin-top: -50px;
     }
@@ -70,8 +54,7 @@ IonListGridComponent.decorators = [
         margin-top: -30px;
         font-size: 12px;
     }   
-    `
-                ]
+    `]
             },] },
 ];
 /** @nocollapse */
--- src/components/ion-list-grid/ion-list-grid.ts
+++ src/components/ion-list-grid/ion-list-grid.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 /**
  * Generated class for the IonListCategoryComponent component.
@@ -7,48 +7,38 @@ import { Component, Input } from "@angular/core";
  * Components.
  */
 @Component({
-  selector: "ion-list-grid",
-  template: `
+    selector: 'ion-list-grid',
+    template: `
     <ion-grid>
     <ion-row>
       <ion-col col-6 *ngIf="items && items.length > 0">
         <img src="{{items[0].image}}">
-        <div class="p-margin-top-50">
-        <p>{{items[0].name}}</p>
-        <p>{{items[0].price}}</p>
-        </div>
+        <p class="p-margin-top-50">{{items[0].name}}</p>
+        <p class="p-price">{{items[0].price}}</p>        
       </ion-col>
       <ion-col col-6>
         <ion-row>
           <ion-col col-6 *ngIf="items && items.length > 1">
             <img src="{{items[1].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[1].name}}</p>
-            <p>{{items[1].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[1].name}}</p>
+            <p class="p-price">{{items[1].price}}</p>
           </ion-col>
           <ion-col col-6 *ngIf="items && items.length > 2">
             <img src="{{items[2].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[2].name}}</p>
-            <p>{{items[2].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[2].name}}</p>
+            <p class="p-price">{{items[2].price}}</p>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col col-6 *ngIf="items && items.length > 3">
             <img src="{{items[3].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[3].name}}</p>
-            <p>{{items[3].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[3].name}}</p>
+            <p class="p-price">{{items[3].price}}</p>      
           </ion-col>
           <ion-col col-6 *ngIf="items && items.length > 4">
             <img src="{{items[4].image}}">
-            <div class="p-margin-top-40">
-            <p>{{items[4].name}}</p>
-            <p>{{items[4].price}}</p>
-            </div>
+            <p class="p-margin-top-40">{{items[4].name}}</p>
+            <p class="p-price">{{items[4].price}}</p>
           </ion-col>
         </ion-row>
       </ion-col>
@@ -56,8 +46,7 @@ import { Component, Input } from "@angular/core";
   </ion-grid>
   
     `,
-  styles: [
-    `
+    styles: [`
     .p-margin-top-50 {
       margin-top: -50px;
     }
@@ -65,13 +54,13 @@ import { Component, Input } from "@angular/core";
         margin-top: -30px;
         font-size: 12px;
     }   
-    `
-  ]
+    `]
 })
 export class IonListGridComponent {
-  @Input() items: Array<any>;
+    @Input() items: Array<any>;
 
-  constructor() {
-    // console.log('Hello IonFormShopComponent Component');
-  }
+    constructor() {
+        // console.log('Hello IonFormShopComponent Component');
+    }
+
 }
