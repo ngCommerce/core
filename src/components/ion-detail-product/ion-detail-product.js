76934058af1e85bbe7bcaf35f0b2f1a69fe9412a
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-05 11:59:40 +0700

    git push

--- src/components/ion-detail-product/ion-detail-product.js
+++ src/components/ion-detail-product/ion-detail-product.js
@@ -141,7 +141,7 @@ IonDetailProductComponent.decorators = [
                 <ion-col width-30>
                     <span>{{group.name}}Star</span>
                 </ion-col>
-                <ion-col width-60 class="progress-bar">
+                <ion-col width-60 class="xxx">
                     <span [style.width]="group.percent"></span>
                 </ion-col>
                 <ion-col width-10>
@@ -185,7 +185,9 @@ IonDetailProductComponent.decorators = [
     `,
                 styles: [`
   ion-detail-product {
-    background-color: red;
+    .xxx{
+        background-color: silver;
+    }
   }`
                 ]
             },] },
