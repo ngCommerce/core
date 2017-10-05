4ac05308751d2f37ad528b68598a315af5c2d3e3
mob35 <toonelicious@gmail.com>
2017-10-05 16:11:52 +0700

    update

--- src/components/ion-form-confirm/ion-form-confirm.ts
+++ src/components/ion-form-confirm/ion-form-confirm.ts
@@ -7,8 +7,8 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
  * Components.
  */
 @Component({
-    selector: 'ion-form-confirm',
-    template: `
+  selector: 'ion-form-confirm',
+  template: `
     <ion-content *ngIf="confirmgateway.order">
     <ion-grid>
       <ion-row>
@@ -60,12 +60,31 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
                     <p>{{item.product.price}} บาท</p>
                   </ion-col>
                 </ion-row>
+
+
+
                 <ion-row no-padding>
                   <ion-col no-padding width-100>
                     <p>{{item.delivery.name}}</p>
                     <p>{{item.delivery.detail}}</p>
                   </ion-col>
                 </ion-row>
+
+                <ion-row>
+                  <ion-col width-100>
+                    <p>{{'วิธีจัดส่ง'}}</p>
+                  </ion-col>
+                </ion-row>
+
+                <ion-row>
+                <ion-col width-100>
+                <ion-label>
+                <p>{{item.delivery.name}}</p>
+                <p>{{item.delivery.detail}}</p>
+                </ion-label>
+                </ion-col>
+              </ion-row>
+
               </ion-item>
             </ion-list>
             <ion-row>
@@ -89,39 +108,39 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
     </ion-grid>
   </ion-content>
     `,
-    styles: [`
+  styles: [`
   ion-form-confirm {
     
   }`
-    ]
+  ]
 })
 export class IonFormConfirmComponent {
-    data: any;
-    @Input() confirmgateway: any;
-    @Output() gotoNext: EventEmitter<any> = new EventEmitter<any>();
-    confirmdiscount: number;
-    constructor() {
-        // console.log('Hello IonFormConfirmComponent Component');
+  data: any;
+  @Input() confirmgateway: any;
+  @Output() gotoNext: EventEmitter<any> = new EventEmitter<any>();
+  confirmdiscount: number;
+  constructor() {
+    // console.log('Hello IonFormConfirmComponent Component');
+  }
+  discount(data) {
+    if (data && data > 0) {
+      if (this.confirmgateway.order.amount && this.confirmgateway.order.amount >= data) {
+        this.confirmgateway.order.discount = data;
+      } else {
+        this.confirmgateway.order.discount = this.confirmgateway.order.amount;
+      }
+    } else {
+      this.confirmgateway.order.discount = 0;
     }
-    discount(data) {
-        if (data && data > 0) {
-            if (this.confirmgateway.order.amount && this.confirmgateway.order.amount >= data) {
-                this.confirmgateway.order.discount = data;
-            } else {
-                this.confirmgateway.order.discount = this.confirmgateway.order.amount;
-            }
-        } else {
-            this.confirmgateway.order.discount = 0;
-        }
-    }
-    stepValidation() {
+  }
+  stepValidation() {
 
-        if (this.confirmgateway.order.discount && this.confirmgateway.order.discount > 0 && !undefined) {
-            this.confirmgateway.order.totalamount = ((this.confirmgateway.order.deliveryprice || 0) + (this.confirmgateway.order.amount || 0)) - (this.confirmgateway.order.discount || 0);
-        } else {
-            this.confirmgateway.order.totalamount = ((this.confirmgateway.order.deliveryprice || 0) + (this.confirmgateway.order.amount || 0));
-        }
-        this.gotoNext.emit(this.confirmgateway.order);
+    if (this.confirmgateway.order.discount && this.confirmgateway.order.discount > 0 && !undefined) {
+      this.confirmgateway.order.totalamount = ((this.confirmgateway.order.deliveryprice || 0) + (this.confirmgateway.order.amount || 0)) - (this.confirmgateway.order.discount || 0);
+    } else {
+      this.confirmgateway.order.totalamount = ((this.confirmgateway.order.deliveryprice || 0) + (this.confirmgateway.order.amount || 0));
     }
+    this.gotoNext.emit(this.confirmgateway.order);
+  }
 
 }
