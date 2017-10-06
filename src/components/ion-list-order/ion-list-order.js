652953e2b52319b17c6e3d8ee995b6e98396bb75
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-06 14:25:23 +0700

    1.1.94

--- src/components/ion-list-order/ion-list-order.js
+++ src/components/ion-list-order/ion-list-order.js
@@ -28,7 +28,7 @@ IonListOrderComponent.decorators = [
                 template: `
       <ion-list *ngIf="items">
         <ion-item *ngFor="let item of items; let i = index" (click)="selectOrder(item)">
-      <p>Ord : {{item.order._id}}</p>
+      <p>Ord : {{item.order_id}}</p>
             <ion-thumbnail item-start>
             <img src="{{item.image}}">
         </ion-thumbnail>
