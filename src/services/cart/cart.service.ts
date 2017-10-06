348b89d2006f24f7b9eb92eed9be57e008e38037
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-06 16:43:07 +0700

    update cart

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -107,10 +107,11 @@ export class CartService {
     }
 
     onCalculate(cart) {
-        cart.amount = 0;
-        cart.discount = 0;
-        cart.totalamount = 0;
+        
         if (cart && cart.items) {
+            cart.amount = 0;
+            cart.discount = 0;
+            cart.totalamount = 0;
             for (var i = 0; i < cart.items.length; i++) {
                 let item = cart.items[i];
                 let promotionprice = item.product.promotionprice ? item.product.promotionprice : 0;
@@ -123,7 +124,6 @@ export class CartService {
                 cart.totalamount += item.totalamount;
             }
         }
-
 
         return cart;
 
