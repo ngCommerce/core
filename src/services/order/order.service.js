df36156ae54e7be804effc8ec720572646bd52f1
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-06 10:34:09 +0700

    1.1.83

--- src/services/order/order.service.js
+++ src/services/order/order.service.js
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from "@angular/core";
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import { Http } from "@angular/http";
+import "rxjs/add/operator/toPromise";
 import { API_URL } from "../../models/core.model";
 import { CorService } from "../../core.service";
 export class OrderService {
@@ -11,70 +11,86 @@ export class OrderService {
     }
     getOrderList() {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.get(this._apiURL + 'orders/', { headers: headers })
+        return this.http
+            .get(this._apiURL + "orders/", { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     createOrder(Order) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.post(this._apiURL + 'orders/', Order, { headers: headers })
+        return this.http
+            .post(this._apiURL + "orders/", Order, { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     getOrderByID(id) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.get(this._apiURL + 'orders/' + id, { headers: headers })
+        return this.http
+            .get(this._apiURL + "orders/" + id, { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     updateOrder(Order) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'orders/' + Order._id, Order, { headers: headers })
+        return this.http
+            .put(this._apiURL + "orders/" + Order._id, Order, { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     deleteOrder(id) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.delete(this._apiURL + 'orders/' + id, { headers: headers })
+        return this.http
+            .delete(this._apiURL + "orders/" + id, { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     getOrderByShop() {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.get(this._apiURL + 'orderbyshop', { headers: headers })
+        return this.http
+            .get(this._apiURL + "orderbyshop", { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     updateItemToAccept(orderId, itemId) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateorderaccept/' + orderId + '/' + itemId, { headers: headers })
+        return this.http
+            .put(this._apiURL + "updateorderaccept/" + orderId + "/" + itemId, null, {
+            headers: headers
+        })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     updateItemToSent(orderId, itemId) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateordersent/' + orderId + '/' + itemId, { headers: headers })
+        return this.http
+            .put(this._apiURL + "updateordersent/" + orderId + "/" + itemId, null, {
+            headers: headers
+        })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     updateItemToComplete(orderId, itemId) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateordercomplete/' + orderId + '/' + itemId, { headers: headers })
+        return this.http
+            .put(this._apiURL + "updateordercomplete/" + orderId + "/" + itemId, null, { headers: headers })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
     }
     updateItemToReject(orderId, itemId) {
         let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateorderreject/' + orderId + '/' + itemId, { headers: headers })
+        return this.http
+            .put(this._apiURL + "updateorderreject/" + orderId + "/" + itemId, null, {
+            headers: headers
+        })
             .toPromise()
             .then(response => response.json())
             .catch(this.handleError);
--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -1,100 +1,122 @@
 import { Injectable, Inject } from "@angular/core";
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import { Http } from "@angular/http";
+import "rxjs/add/operator/toPromise";
 import { API_URL } from "../../models/core.model";
 import { CorService } from "../../core.service";
 import { OrderModel, ItemByOrderByShopModel } from "../../models/Order.model";
 
 @Injectable()
 export class OrderService {
-    private _apiURL: String;
-    constructor( @Inject(API_URL) apiURL: String, public http: Http, public corService: CorService) {
-        this._apiURL = apiURL;
-    }
+  private _apiURL: String;
+  constructor(
+    @Inject(API_URL) apiURL: String,
+    public http: Http,
+    public corService: CorService
+  ) {
+    this._apiURL = apiURL;
+  }
 
-    getOrderList(): Promise<Array<OrderModel>> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.get(this._apiURL + 'orders/', { headers: headers })
-            .toPromise()
-            .then(response => response.json() as Array<OrderModel>)
-            .catch(this.handleError);
-    }
+  getOrderList(): Promise<Array<OrderModel>> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .get(this._apiURL + "orders/", { headers: headers })
+      .toPromise()
+      .then(response => response.json() as Array<OrderModel>)
+      .catch(this.handleError);
+  }
 
-    createOrder(Order): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.post(this._apiURL + 'orders/', Order, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  createOrder(Order): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .post(this._apiURL + "orders/", Order, { headers: headers })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    getOrderByID(id): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.get(this._apiURL + 'orders/' + id, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  getOrderByID(id): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .get(this._apiURL + "orders/" + id, { headers: headers })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    updateOrder(Order): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'orders/' + Order._id, Order, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  updateOrder(Order): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .put(this._apiURL + "orders/" + Order._id, Order, { headers: headers })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    deleteOrder(id): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.delete(this._apiURL + 'orders/' + id, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  deleteOrder(id): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .delete(this._apiURL + "orders/" + id, { headers: headers })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    getOrderByShop(): Promise<ItemByOrderByShopModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.get(this._apiURL + 'orderbyshop', { headers: headers })
-            .toPromise()
-            .then(response => response.json() as ItemByOrderByShopModel)
-            .catch(this.handleError);
-    }
+  getOrderByShop(): Promise<ItemByOrderByShopModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .get(this._apiURL + "orderbyshop", { headers: headers })
+      .toPromise()
+      .then(response => response.json() as ItemByOrderByShopModel)
+      .catch(this.handleError);
+  }
 
-    updateItemToAccept(orderId, itemId): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateorderaccept/' + orderId + '/' + itemId, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  updateItemToAccept(orderId, itemId): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .put(this._apiURL + "updateorderaccept/" + orderId + "/" + itemId, null, {
+        headers: headers
+      })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    updateItemToSent(orderId, itemId): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateordersent/' + orderId + '/' + itemId, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  updateItemToSent(orderId, itemId): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .put(this._apiURL + "updateordersent/" + orderId + "/" + itemId, null, {
+        headers: headers
+      })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    updateItemToComplete(orderId, itemId): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateordercomplete/' + orderId + '/' + itemId, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  updateItemToComplete(orderId, itemId): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .put(
+        this._apiURL + "updateordercomplete/" + orderId + "/" + itemId,
+        null,
+        { headers: headers }
+      )
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-    updateItemToReject(orderId, itemId): Promise<OrderModel> {
-        let headers = this.corService.createAuthorizationHeader();
-        return this.http.put(this._apiURL + 'updateorderreject/' + orderId + '/' + itemId, { headers: headers })
-            .toPromise()
-            .then(response => response.json() as OrderModel)
-            .catch(this.handleError);
-    }
+  updateItemToReject(orderId, itemId): Promise<OrderModel> {
+    let headers = this.corService.createAuthorizationHeader();
+    return this.http
+      .put(this._apiURL + "updateorderreject/" + orderId + "/" + itemId, null, {
+        headers: headers
+      })
+      .toPromise()
+      .then(response => response.json() as OrderModel)
+      .catch(this.handleError);
+  }
 
-
-
-    private handleError(error: any): Promise<any> {
-        return Promise.reject(error.message || error);
-    }
+  private handleError(error: any): Promise<any> {
+    return Promise.reject(error.message || error);
+  }
 }
