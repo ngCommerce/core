39e197c0651fb4a8f52ad41a1397c10e9a99834e
mob35 <toonelicious@gmail.com>
2017-10-09 11:18:49 +0700

    update

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -1,6 +1,6 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { ProductModel } from '../../index';
-import { IonUploadImageComponent } from "../ion-upload-image/ion-upload-image";
+import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image";
 
 /**
  * Generated class for the IonListCategoryComponent component.
