d9947f86fd0f413f1de94eff2b1b8d78fe28ece9
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-09 11:33:19 +0700

    update ion upload image

--- src/components/ion-form-product/ion-form-product.js
+++ src/components/ion-form-product/ion-form-product.js
@@ -74,10 +74,10 @@ IonFormProductComponent.decorators = [
           <ion-option *ngFor="let item of shops" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
-    
-      <ion-upload-image (imageOutList)="imageList($event)"></ion-upload-image>
       
     </ion-list>
+    <p>Upload images</p>
+    <ion-upload-image (imageOutList)="imageList($event)"></ion-upload-image>
     
     <div padding>
       <button ion-button block (click)="onClick(item)">Submit</button>
