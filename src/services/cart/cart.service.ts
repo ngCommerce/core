fc6f012757d8f6b34e470f10cb7dafe449536ea5
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-09 15:46:11 +0700

    update

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -79,13 +79,12 @@ export class CartService {
             }
 
         }
-        if (!cartStorage) {
-            if (!cartStorage.items) {
-                cartStorage = {
-                    items: []
-                }
+        if (!cartStorage && !cartStorage.items) {
+            cartStorage = {
+                items: []
             }
         }
+        
         cartStorage.items.push({
             product: product,
             qty: 1,
