a26cff49886bc1ea92e6a951129b2984151a0028
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-09 15:31:51 +0700

    update

--- src/services/cart/cart.service.ts
+++ src/services/cart/cart.service.ts
@@ -62,7 +62,7 @@ export class CartService {
 
 
     addToCart(product) {
-        let cartStorage = window.localStorage.getItem('gCart') ? JSON.parse(window.localStorage.getItem('gCart')) : null;
+        let cartStorage = window.localStorage.getItem('gCart') ? JSON.parse(window.localStorage.getItem('gCart')) : [];
         if (cartStorage && cartStorage.items && cartStorage.items.length > 0) {
 
             for (var i = 0; i < cartStorage.items.length; i++) {
