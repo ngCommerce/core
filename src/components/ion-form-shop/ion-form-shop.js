a3b939073004b702bf080380be4b2851593ec1a7
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-09 13:59:20 +0700

    1.2.19

--- src/components/ion-form-shop/ion-form-shop.js
+++ src/components/ion-form-shop/ion-form-shop.js
@@ -14,6 +14,9 @@ export class IonFormShopComponent {
     addShop(item) {
         this.createShop.emit(item);
     }
+    imageList(e) {
+        this.item.image = e[0];
+    }
 }
 IonFormShopComponent.decorators = [
     { type: Component, args: [{
@@ -24,10 +27,9 @@ IonFormShopComponent.decorators = [
             <ion-label floating >Name</ion-label>
             <ion-input type="text" [(ngModel)]="item.name"></ion-input>
         </ion-item>
-        <ion-item>
-            <ion-label floating >Image</ion-label>
-            <ion-input type="text" [(ngModel)]="item.image"></ion-input>
-        </ion-item>
+
+        <p>Upload images</p>
+        <ion-upload-image [maxImage]="1" (imageOutList)="imageList($event)"></ion-upload-image>
         <button ion-button block color="dark" (click)="addShop(item)">Add</button>
     </ion-list>
     `,
