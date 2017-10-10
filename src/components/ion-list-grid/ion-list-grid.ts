8f7422a6b5a8d35dd6fed3ab5513495a52d285b1
nicknameismos <mynameissarawut@gmail.com>
2017-10-10 17:16:57 +0700

    ion-list-grid

--- src/components/ion-list-grid/ion-list-grid.ts
+++ src/components/ion-list-grid/ion-list-grid.ts
@@ -7,35 +7,44 @@ import { Component, Input } from '@angular/core';
  * Components.
  */
 @Component({
-    selector: 'ion-list-grid',
-    template: `
+  selector: 'ion-list-grid',
+  template: `
     <ion-grid>
-    <ion-row>
-      <ion-col col-6 *ngIf="items && items.length > 0">
+
+    <ion-row *ngIf="items && items.length === 1">
+    <ion-col col-12>
+      <img src="{{items[0].image}}">
+      <p class="p-margin-top-50">{{items[0].name}}</p>
+      <p class="p-price-50">{{items[0].price}}</p>        
+    </ion-col>
+  </ion-row>
+
+    <ion-row *ngIf="items && items.length === 5">
+      <ion-col col-6>
         <img src="{{items[0].image}}">
         <p class="p-margin-top-50">{{items[0].name}}</p>
         <p class="p-price-50">{{items[0].price}}</p>        
       </ion-col>
       <ion-col col-6>
         <ion-row>
-          <ion-col col-6 *ngIf="items && items.length > 1">
+          <ion-col col-6>
             <img src="{{items[1].image}}">
             <p class="p-margin-top-40">{{items[1].name}}</p>
             <p class="p-price-40">{{items[1].price}}</p>
           </ion-col>
-          <ion-col col-6 *ngIf="items && items.length > 2">
+          <ion-col col-6>
             <img src="{{items[2].image}}">
             <p class="p-margin-top-40">{{items[2].name}}</p>
             <p class="p-price-40">{{items[2].price}}</p>
           </ion-col>
         </ion-row>
         <ion-row>
-          <ion-col col-6 *ngIf="items && items.length > 3">
+          <ion-col col-6>
             <img src="{{items[3].image}}">
             <p class="p-margin-top-40">{{items[3].name}}</p>
             <p class="p-price-40">{{items[3].price}}</p>      
           </ion-col>
-          <ion-col col-6 *ngIf="items && items.length > 4">
+          <ion-col col-6>
             <img src="{{items[4].image}}">
             <p class="p-margin-top-40">{{items[4].name}}</p>
             <p class="p-price-40">{{items[4].price}}</p>
@@ -43,10 +52,24 @@ import { Component, Input } from '@angular/core';
         </ion-row>
       </ion-col>
     </ion-row>
+
+    <ion-row *ngIf="items && items.length === 2">
+    <ion-col col-6>
+      <img src="{{items[0].image}}">
+      <p class="p-margin-top-50">{{items[0].name}}</p>
+      <p class="p-price-50">{{items[0].price}}</p>        
+    </ion-col>
+    <ion-col col-6>
+    <img src="{{items[1].image}}">
+    <p class="p-margin-top-50">{{items[1].name}}</p>
+    <p class="p-price-50">{{items[1].price}}</p>  
+    </ion-col>
+  </ion-row>
+
   </ion-grid>
   
     `,
-    styles: [`
+  styles: [`
     .p-margin-top-50 {
       margin-top: -50px;
     }
@@ -57,10 +80,10 @@ import { Component, Input } from '@angular/core';
     `]
 })
 export class IonListGridComponent {
-    @Input() items: Array<any>;
+  @Input() items: Array<any>;
 
-    constructor() {
-        // console.log('Hello IonFormShopComponent Component');
-    }
+  constructor() {
+    // console.log('Hello IonFormShopComponent Component');
+  }
 
 }
