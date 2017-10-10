bbd38b75cff07dd83877619c42107fadeb74b306
abhisit snk <abhisit2537@hotmail.com>
2017-10-10 14:31:17 +0700

    update

--- src/components/ion-list-product/ion-list-product.ts
+++ src/components/ion-list-product/ion-list-product.ts
@@ -9,22 +9,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'ion-list-product',
   template: `
-    <ion-list>
-    <ion-item-sliding *ngFor="let item of items" (click)="add(item)">
-      <ion-item>
-      <ion-thumbnail item-start>
-      <img src="{{item.image}}">
-  </ion-thumbnail>
-  <h2>{{item.name}}</h2>
-  <p>{{item.price | number}} {{item.currency}}</p>
-  <rating [(ngModel)]="item.rate" readOnly="false"  max="5"  emptyStarIconName="star-outline" halfStarIconName="star-half"starIconName="star" nullable="false"></rating>
-
+    <ion-list >
+      <ion-item *ngFor="let item of items" (click)="add(item)">
+        <ion-thumbnail item-start>
+            <img src="{{item.image}}">
+        </ion-thumbnail>
+        <h2>{{item.name}}</h2>
+        <p>{{item.price | number}} {{item.currency}}</p>
+        <rating [(ngModel)]="item.rate" readOnly="false"  max="5"  emptyStarIconName="star-outline" halfStarIconName="star-half"starIconName="star" nullable="false"></rating>
       </ion-item>
-      <ion-item-options side="right">
-      <button ion-button color="danger" (click)="deleteProduct(item)">
-      </ion-item-options>
-    </ion-item-sliding>
-  </ion-list>
+    </ion-list>
     `,
   styles: [`
   ion-list-product {
@@ -35,17 +29,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class IonListProductComponent {
   @Input() items: any;
   @Output() selectedProduct: EventEmitter<any> = new EventEmitter<any>();
-  @Output() delete: EventEmitter<any> = new EventEmitter<any>();
   constructor() {
     // console.log('Hello IonListProductComponent Component');
   }
   add(item) {
     this.selectedProduct.emit(item);
   }
-  deleteProduct(item){
-    this.delete.emit(item._id)
-  }
-
-
 
 }
