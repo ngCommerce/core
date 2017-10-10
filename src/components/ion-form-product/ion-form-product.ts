03e7348abe6f2eea400b0c1399ab09ce3307e3cd
abhisit snk <abhisit2537@hotmail.com>
2017-10-10 17:21:38 +0700

    update

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -15,51 +15,51 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
     
       <ion-item>
         <ion-label floating>Name*</ion-label>
-        <ion-input type="text" value="{{product.name}}" [(ngModel)]="item.name"></ion-input>
+        <ion-input type="text" [(ngModel)]="product.name"></ion-input>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Detail*</ion-label>
-        <ion-input type="text"  value="{{product.detail}}" [(ngModel)]="item.detail"></ion-input>
+        <ion-input type="text" [(ngModel)]="product.detail"></ion-input>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Price*</ion-label>
-        <ion-input type="number"  value="{{product.price}}" [(ngModel)]="item.price"></ion-input>
+        <ion-input type="number" [(ngModel)]="product.price"></ion-input>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Promotion Price</ion-label>
-        <ion-input type="number"  value="{{product.promotionprice}}" [(ngModel)]="item.promotionprice"></ion-input>
+        <ion-input type="number" [(ngModel)]="product.promotionprice"></ion-input>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Percent of discount</ion-label>
-        <ion-input type="number"  value="{{product.percentofdiscount}}" [(ngModel)]="item.percentofdiscount"></ion-input>
+        <ion-input type="number" [(ngModel)]="product.percentofdiscount"></ion-input>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Currency*</ion-label>
-        <ion-input type="text"   value="{{product.currency}}" [(ngModel)]="item.currency"></ion-input>
+        <ion-input type="text" [(ngModel)]="product.currency"></ion-input>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Categories*</ion-label>
-        <ion-select  [(ngModel)]="item.categories" multiple="true">
+        <ion-select  [(ngModel)]="product.categories" multiple="true">
           <ion-option *ngFor="let item of categories" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Shippings*</ion-label>
-        <ion-select  [(ngModel)]="item.shippings" multiple="true">
+        <ion-select  [(ngModel)]="product.shippings" multiple="true">
           <ion-option *ngFor="let item of shippings" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
     
       <ion-item>
         <ion-label floating>Shop*</ion-label>
-        <ion-select [(ngModel)]="item.shop">
+        <ion-select [(ngModel)]="product.shop">
           <ion-option *ngFor="let item of shops" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
@@ -70,7 +70,7 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
     </ion-list>
     
     <div padding>
-      <button ion-button block (click)="onClick(item)">Submit</button>
+      <button ion-button block (click)="onClick(product)">Submit</button>
     </div>
     
     `,
@@ -88,7 +88,7 @@ export class IonFormProductComponent {
   @Input() shops: any;
   @Output() itemClicked: EventEmitter<any> = new EventEmitter<any>();
   constructor() {
-    this.product = this.item;
+    this.product = JSON.parse(JSON.stringify(this.item));
     // console.log('Hello IonListCategoryComponent Component');
   }
   onClick(item) {
