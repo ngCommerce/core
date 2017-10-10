cdce088aea88b1b4fb919d809f63480474048556
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-10 18:16:26 +0700

    1.2.63

--- src/components/ion-list-grid/ion-list-grid.js
+++ src/components/ion-list-grid/ion-list-grid.js
@@ -15,7 +15,7 @@ IonListGridComponent.decorators = [
                 selector: 'ion-list-grid',
                 template: `
   <ion-grid>
-  <ion-row>
+  <ion-row *ngIf="items && items.length > 5">
     <ion-col col-6 *ngIf="items && items.length > 0">
       <img src="{{items[0].image}}">
       <p class="p-margin-top-50">{{items[0].name}} {{items[0].price}}</p>
