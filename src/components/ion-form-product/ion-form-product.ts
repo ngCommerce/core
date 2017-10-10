4500fa136ed360ce31541b096b7f1226c127f9d5
abhisit snk <abhisit2537@hotmail.com>
2017-10-10 18:28:25 +0700

    asd

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -59,7 +59,7 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
       <ion-item>
         <ion-label floating>Shop*</ion-label>
         <ion-select [(ngModel)]="item.shop">
-          <ion-option *ngFor="let items of shops" [value]="items.name">{{items.name}}</ion-option>
+          <ion-option *ngFor="let items of shops" [value]="items.name" [selected]="items === item.shop">{{items.name}}</ion-option>
         </ion-select>
       </ion-item>
 
