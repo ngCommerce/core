5f09b5ef2b8e99798b6e75dae4d69854d57fb529
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-10 11:18:58 +0700

    update required create product

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -14,17 +14,17 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
     <ion-list>
     
       <ion-item>
-        <ion-label floating>Name</ion-label>
+        <ion-label floating>Name*</ion-label>
         <ion-input type="text" [(ngModel)]="item.name"></ion-input>
       </ion-item>
     
       <ion-item>
-        <ion-label floating>Detail</ion-label>
+        <ion-label floating>Detail*</ion-label>
         <ion-input type="text" [(ngModel)]="item.detail"></ion-input>
       </ion-item>
     
       <ion-item>
-        <ion-label floating>Price</ion-label>
+        <ion-label floating>Price*</ion-label>
         <ion-input type="number" [(ngModel)]="item.price"></ion-input>
       </ion-item>
     
@@ -39,32 +39,32 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
       </ion-item>
     
       <ion-item>
-        <ion-label floating>Currency</ion-label>
+        <ion-label floating>Currency*</ion-label>
         <ion-input type="text" [(ngModel)]="item.currency"></ion-input>
       </ion-item>
     
       <ion-item>
-        <ion-label floating>Categories</ion-label>
+        <ion-label floating>Categories*</ion-label>
         <ion-select [(ngModel)]="item.categories" multiple="true">
           <ion-option *ngFor="let item of categories" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
     
       <ion-item>
-        <ion-label floating>Shippings</ion-label>
+        <ion-label floating>Shippings*</ion-label>
         <ion-select [(ngModel)]="item.shippings" multiple="true">
           <ion-option *ngFor="let item of shippings" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
     
       <ion-item>
-        <ion-label floating>Shop</ion-label>
+        <ion-label floating>Shop*</ion-label>
         <ion-select [(ngModel)]="item.shop">
           <ion-option *ngFor="let item of shops" [value]="item">{{item.name}}</ion-option>
         </ion-select>
       </ion-item>
 
-      <p id="productImg">Images</p>
+      <p id="productImg">Images*</p>
       <ion-upload-image [maxImage]="5" (imageOutList)="imageList($event)"></ion-upload-image>
       
     </ion-list>
@@ -90,6 +90,33 @@ export class IonFormProductComponent {
     // console.log('Hello IonListCategoryComponent Component');
   }
   onClick(item) {
+
+    if (!item.name) {
+      alert('Please Enter Your Name!');
+      return;
+    } else if (!item.detail) {
+      alert('Please Enter Your Detail!');
+      return;
+    } else if (!item.price) {
+      alert('Please Enter Your Price!');
+      return;
+    } else if (!item.currency) {
+      alert('Please Enter Your Currency!');
+      return;
+    } else if (!item.shippings) {
+      alert('Please Enter Your Shippings!');
+      return;
+    } else if (!item.categories) {
+      alert('Please Enter Your Categories!');
+      return;
+    } else if (!item.shop) {
+      alert('Please Enter Your Shop!');
+      return;
+    } else if (!this.item.images && this.item.images.length === 0) {
+      alert('Please Enter Your Upload Image!');
+      return;
+    }
+
     this.itemClicked.emit(item);
   }
   imageList(e) {
