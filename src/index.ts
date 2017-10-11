810233971cb77804367d5c7643a27713678deeb9
moomam6934 <destinationpainbm@gmail.com>
2017-10-11 11:14:23 +0700

    ion-detail-shop.ts

--- src/index.ts
+++ src/index.ts
@@ -59,6 +59,7 @@ export * from "./components/ion-form-paymentoption/ion-form-paymentoption";
 export * from "./components/ion-detail-order/ion-detail-order";
 export * from "./components/ion-segment-order/ion-segment-order";
 export * from "./components/ion-list-order/ion-list-order";
+export * from "./components/ion-detail-shop/ion-detail-shop";
 export * from "./components/ion-orders/ion-orders"
 export * from "./components/ion-form-review/ion-form-review"
 export * from "./components/ion-upload-image/ion-upload-image"
