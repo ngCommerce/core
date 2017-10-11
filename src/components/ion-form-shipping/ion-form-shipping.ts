cbfde03a8015ab3079f847f7c60b025059867c4f
mob35 <toonelicious@gmail.com>
2017-10-11 16:59:00 +0700

    update

--- src/components/ion-form-shipping/ion-form-shipping.ts
+++ src/components/ion-form-shipping/ion-form-shipping.ts
@@ -65,22 +65,7 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
                     <ion-radio (click)="setproduct(item,itm)"></ion-radio>
                 </ion-item>
             </ion-list>
-        </div>
-        <div *ngIf="listshipping.items && listshipping.items.length > 0">
-        <ion-row>
-            <ion-col width-50>
-                <p>{{'ราคารวม'}}</p>
-                <p>{{'ส่วนลด'}}</p>
-                <p>{{'รวมสุทธิ'}}</p>
-            </ion-col>
-            <ion-col width-50>
-                <p text-right>{{listshipping.amount | number}} {{item.product.currency}}</p>
-                <p text-right>{{listshipping.discount | number}} {{item.product.currency}}</p>
-                <p text-right>{{listshipping.totalamount | number}} {{item.product.currency}}</p>
-            </ion-col>
-        </ion-row>
-    </div>
-    </ion-list>
+        
     <ion-grid>
         <div *ngIf="listshipping.items && listshipping.items.length > 0">
             <ion-row>
@@ -97,6 +82,8 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
             </ion-row>
         </div>
     </ion-grid>
+    </div>
+    </ion-list>
     <ion-grid>
         <button ion-button full color="danger" (click)="stepValidation()">{{'ดำเนินการชำระเงิน'}}</button>
     </ion-grid>
