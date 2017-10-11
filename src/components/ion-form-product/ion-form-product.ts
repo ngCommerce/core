a2d5dfa68ed84452124beccfcdccbbac71ff2f9e
mob35 <toonelicious@gmail.com>
2017-10-11 18:28:52 +0700

    update

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -57,12 +57,19 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
         </ion-select>
       </ion-item>
       
-      <ion-item>
+      <ion-item *ngIf="!item.shop">
         <ion-label floating>Shop*</ion-label>
         <ion-select [(ngModel)]="item.shop">
           <ion-option *ngFor="let items of shops" [value]="items._id">{{items.name}}</ion-option>
           </ion-select>
       </ion-item>
+
+      <ion-item *ngIf="item.shop && item.shop._id">
+      <ion-label floating>Shop*</ion-label>
+      <ion-select [(ngModel)]="item.shop._id">
+        <ion-option *ngFor="let items of shops" [value]="items._id">{{items.name}}</ion-option>
+        </ion-select>
+    </ion-item>
 
       <p id="productImg">Images*</p>
       <ion-upload-image [maxImage]="5" (imageOutList)="imageList($event)"></ion-upload-image>
