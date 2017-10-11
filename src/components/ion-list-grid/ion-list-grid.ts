ff9435ae4213ac263ebab692d9fd32b945bba962
nicknameismos <mynameissarawut@gmail.com>
2017-10-11 09:44:31 +0700

    ion-list-grid

--- src/components/ion-list-grid/ion-list-grid.ts
+++ src/components/ion-list-grid/ion-list-grid.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 /**
  * Generated class for the IonListCategoryComponent component.
@@ -13,7 +13,7 @@ import { Component, Input } from '@angular/core';
     <ion-row *ngIf="items && items.length === 1">
     <ion-col col-2>
     </ion-col>
-    <ion-col col-8 *ngIf="items && items.length > 0">
+    <ion-col col-8 *ngIf="items && items.length > 0" (click)="selectProduct(items)">
       <img src="{{items[0].image}}">
       <p class="p-margin-top-50">{{items[0].name}} {{items[0].price}}</p>
     </ion-col>
@@ -22,28 +22,28 @@ import { Component, Input } from '@angular/core';
   </ion-row>
 
   <ion-row *ngIf="items && items.length === 2">
-    <ion-col col-6 *ngIf="items && items.length > 0">
+    <ion-col col-6 *ngIf="items && items.length > 0" (click)="selectProduct(items)">
       <img src="{{items[0].image}}">
       <p class="p-margin-top-50">{{items[0].name}} {{items[0].price}}</p>
     </ion-col>
-    <ion-col col-6 *ngIf="items && items.length > 1">
+    <ion-col col-6 *ngIf="items && items.length > 1" (click)="selectProduct(items)">
       <img src="{{items[1].image}}">
       <p class="p-margin-top-50">{{items[1].name}} {{items[1].price}}</p>
     </ion-col>
   </ion-row>
 
   <ion-row *ngIf="items && items.length === 3">
-    <ion-col col-6 *ngIf="items && items.length > 0">
+    <ion-col col-6 *ngIf="items && items.length > 0" (click)="selectProduct(items)">
       <img src="{{items[0].image}}">
       <p class="p-margin-top-50">{{items[0].name}} {{items[0].price}}</p>
     </ion-col>
     <ion-col col-6>
       <ion-row>
-        <ion-col col-6 *ngIf="items && items.length > 1">
+        <ion-col col-6 *ngIf="items && items.length > 1" (click)="selectProduct(items)">
           <img src="{{items[1].image}}">
           <p class="p-margin-top-40">{{items[1].name}} {{items[1].price}}</p>
         </ion-col>
-        <ion-col col-6 *ngIf="items && items.length > 2">
+        <ion-col col-6 *ngIf="items && items.length > 2" (click)="selectProduct(items)">
           <img src="{{items[2].image}}">
           <p class="p-margin-top-40">{{items[2].name}} {{items[2].price}}</p>
         </ion-col>
@@ -53,23 +53,23 @@ import { Component, Input } from '@angular/core';
 
 
   <ion-row *ngIf="items && items.length === 4">
-    <ion-col col-6 *ngIf="items && items.length > 0">
+    <ion-col col-6 *ngIf="items && items.length > 0" (click)="selectProduct(items)">
       <img src="{{items[0].image}}">
       <p class="p-margin-top-50">{{items[0].name}} {{items[0].price}}</p>
     </ion-col>
     <ion-col col-6>
       <ion-row>
-        <ion-col col-6 *ngIf="items && items.length > 1">
+        <ion-col col-6 *ngIf="items && items.length > 1" (click)="selectProduct(items)">
           <img src="{{items[1].image}}">
           <p class="p-margin-top-40">{{items[1].name}} {{items[1].price}}</p>
         </ion-col>
-        <ion-col col-6 *ngIf="items && items.length > 2">
+        <ion-col col-6 *ngIf="items && items.length > 2" (click)="selectProduct(items)">
           <img src="{{items[2].image}}">
           <p class="p-margin-top-40">{{items[2].name}} {{items[2].price}}</p>
         </ion-col>
       </ion-row>
       <ion-row>
-        <ion-col col-6 *ngIf="items && items.length > 3">
+        <ion-col col-6 *ngIf="items && items.length > 3" (click)="selectProduct(items)">
           <img src="{{items[3].image}}">
           <p class="p-margin-top-40">{{items[3].name}} {{items[3].price}}</p>
         </ion-col>
@@ -78,31 +78,31 @@ import { Component, Input } from '@angular/core';
   </ion-row>
 
     <ion-row *ngIf="items && items.length > 4">
-      <ion-col col-6 *ngIf="items && items.length > 0">
+      <ion-col col-6 *ngIf="items && items.length > 0" (click)="selectProduct(items)">
         <img src="{{items[0].image}}">
         <p class="p-margin-top-50">{{items[0].name}}</p>
         <p class="p-price-50">{{items[0].price}}</p>        
       </ion-col>
       <ion-col col-6>
         <ion-row>
-          <ion-col col-6 *ngIf="items && items.length > 1">
+          <ion-col col-6 *ngIf="items && items.length > 1" (click)="selectProduct(items)">
             <img src="{{items[1].image}}">
             <p class="p-margin-top-40">{{items[1].name}}</p>
             <p class="p-price-40">{{items[1].price}}</p>
           </ion-col>
-          <ion-col col-6 *ngIf="items && items.length > 2">
+          <ion-col col-6 *ngIf="items && items.length > 2" (click)="selectProduct(items)">
             <img src="{{items[2].image}}">
             <p class="p-margin-top-40">{{items[2].name}}</p>
             <p class="p-price-40">{{items[2].price}}</p>
           </ion-col>
         </ion-row>
         <ion-row>
-          <ion-col col-6 *ngIf="items && items.length > 3">
+          <ion-col col-6 *ngIf="items && items.length > 3" (click)="selectProduct(items)">
             <img src="{{items[3].image}}">
             <p class="p-margin-top-40">{{items[3].name}}</p>
             <p class="p-price-40">{{items[3].price}}</p>      
           </ion-col>
-          <ion-col col-6 *ngIf="items && items.length > 4">
+          <ion-col col-6 *ngIf="items && items.length > 4" (click)="selectProduct(items)">
             <img src="{{items[4].image}}">
             <p class="p-margin-top-40">{{items[4].name}}</p>
             <p class="p-price-40">{{items[4].price}}</p>
@@ -125,9 +125,13 @@ import { Component, Input } from '@angular/core';
 })
 export class IonListGridComponent {
     @Input() items: Array<any>;
-
+    @Output() itemClicked: EventEmitter<any> = new EventEmitter<any>();
+    
     constructor() {
         // console.log('Hello IonFormShopComponent Component');
     }
-
+    selectProduct(item) {
+      this.itemClicked.emit(item);
+    }
+    
 }
