1e9473d84af5dd628136ad74f0414cbc4d5eec2a
mob35 <toonelicious@gmail.com>
2017-10-11 10:23:01 +0700

    update

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -58,7 +58,7 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
       </ion-item>
       <ion-item *ngIf="shops && shops.length > 0">
         <ion-label floating>Shop*</ion-label>
-        <ion-select [(ngModel)]="shops[0]">
+        <ion-select [(ngModel)]="shops">
           <ion-option *ngFor="let items of shops" [value]="items">{{items.name}}</ion-option>
           </ion-select>
       </ion-item>
@@ -87,7 +87,8 @@ export class IonFormProductComponent {
   @Output() itemClicked: EventEmitter<any> = new EventEmitter<any>();
   constructor() {
     // console.log('Hello IonListCategoryComponent Component');
-    this.item.shop = this.shops[0];
+    console.log(JSON.stringify(this.shops));
+    // this.item.shop = this.shops[0];
   }
 
   checkedShop() {
