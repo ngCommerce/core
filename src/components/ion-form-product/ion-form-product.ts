bd129c0259316edd6ff0a8c8d224184592c6f0ee
mob35 <toonelicious@gmail.com>
2017-10-11 17:58:09 +0700

    up

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -43,19 +43,8 @@ import { IonUploadImageComponent } from "./../ion-upload-image/ion-upload-image"
         <ion-input type="text" [(ngModel)]="item.currency"></ion-input>
       </ion-item>
     
-      <ion-item>
-        <ion-label floating>Categories*</ion-label>
-        <ion-select [(ngModel)]="item.categories" multiple="true">
-          <ion-option *ngFor="let items of categories" [value]="items._id">{{items.name}}</ion-option>
-        </ion-select>
-      </ion-item>
-    
-      <ion-item>
-        <ion-label floating>Shippings*</ion-label>
-        <ion-select [(ngModel)]="item.shippings" multiple="true">
-          <ion-option *ngFor="let items of shippings" [value]="items">{{items.name}}</ion-option>
-        </ion-select>
-      </ion-item>
+     
+
       <ion-item>
         <ion-label floating>Shop*</ion-label>
         <ion-select [(ngModel)]="item.shop._id">
