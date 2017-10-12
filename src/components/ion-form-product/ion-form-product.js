182b4e2465029b2993ea5f8bc6d3f31ca8e8827c
Mac mini <macmini@Macs-Mac-mini.local>
2017-10-12 16:09:45 +0700

    1.3.69

--- src/components/ion-form-product/ion-form-product.js
+++ src/components/ion-form-product/ion-form-product.js
@@ -117,7 +117,7 @@ IonFormProductComponent.decorators = [
       
       <ion-item *ngIf="!item.shop">
         <ion-label floating>Shop*</ion-label>
-        <ion-select [(ngModel)]="item.shop._id">
+        <ion-select [(ngModel)]="item.shop" >
           <ion-option *ngFor="let items of shops" [value]="items._id">{{items.name}}</ion-option>
           </ion-select>
       </ion-item>
