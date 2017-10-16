a8eba2e860bd409a745643358f4ad821bc79fc88
mob35 <toonelicious@gmail.com>
2017-10-16 17:02:50 +0700

    up

--- src/components/ion-upload-image/ion-upload-image.ts
+++ src/components/ion-upload-image/ion-upload-image.ts
@@ -41,6 +41,7 @@ export class IonUploadImageComponent {
     @Output() imageOutList: EventEmitter<any> = new EventEmitter<any>();
 
     constructor(public uploadXServiceProvider: UploadImageService, public imagePicker: ImagePicker, public base64: Base64, public loadingCtrl: LoadingController) {
+        alert('editImg');
         if (this.editImg) {
             for (let i = 0; i < this.editImg.length; i++) {
                 this.imageList.push({
