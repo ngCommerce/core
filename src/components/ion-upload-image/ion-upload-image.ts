55c22e0b5603a3b460c6711e2b0aa762474fd227
mob35 <toonelicious@gmail.com>
2017-10-16 17:50:11 +0700

    update

--- src/components/ion-upload-image/ion-upload-image.ts
+++ src/components/ion-upload-image/ion-upload-image.ts
@@ -3,6 +3,7 @@ import { ImagePicker } from "@ionic-native/image-picker";
 import { Base64 } from "@ionic-native/base64";
 import { UploadImageService } from './../../services/uploadimage/uploadimage.service';
 import { LoadingController } from 'ionic-angular';
+import { ProductModel } from './../../models/product.model';
 
 /**
  * Generated class for the IonListCategoryComponent component.
@@ -37,22 +38,22 @@ export class IonUploadImageComponent {
     public imageList: Array<any> = [];
     public allowUpload: number = 0;
     @Input() maxImage: number;
-    @Input() editImg: Array<any>;
+    @Input() editImg = {} as ProductModel;
     @Output() imageOutList: EventEmitter<any> = new EventEmitter<any>();
 
     constructor(public uploadXServiceProvider: UploadImageService, public imagePicker: ImagePicker, public base64: Base64, public loadingCtrl: LoadingController) {
         alert(JSON.stringify(this.editImg));
         
-        if (this.editImg) {
-            alert(JSON.stringify(this.editImg));
-            for (let i = 0; i < this.editImg.length; i++) {
-                this.imageList.push({
-                    imgUrl: this.editImg[i]
-                });
-            }
-        } else {
-            alert('no img');
-        }
+        // if (this.editImg) {
+        //     alert(JSON.stringify(this.editImg));
+        //     for (let i = 0; i < this.editImg.length; i++) {
+        //         this.imageList.push({
+        //             imgUrl: this.editImg[i]
+        //         });
+        //     }
+        // } else {
+        //     alert('no img');
+        // }
 
     }
     uploadImage() {
