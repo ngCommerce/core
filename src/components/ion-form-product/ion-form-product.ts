5f073956b22575eba19ffc56e626f859728f1a20
mob35 <toonelicious@gmail.com>
2017-10-17 18:18:57 +0700

    update

--- src/components/ion-form-product/ion-form-product.ts
+++ src/components/ion-form-product/ion-form-product.ts
@@ -214,7 +214,7 @@ export class IonFormProductComponent {
   }
 
   chkNumber(num,field) {
-    let nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    let nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let status = false;
     if(num !== null){
 
