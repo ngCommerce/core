f2a13e95d820fa5809b5af95daab401dee3889c4
bu4y <amnes1a@windowslive.com>
2017-10-18 15:16:31 +0700

    1.4.37

--- src/components/ion-detail-product/ion-detail-product.d.ts
+++ src/components/ion-detail-product/ion-detail-product.d.ts
@@ -8,6 +8,7 @@ import { EventEmitter } from '@angular/core';
 export declare class IonDetailProductComponent {
     item: any;
     isReview: Boolean;
+    isIcon: Boolean;
     selectedFavorite: EventEmitter<any>;
     review: EventEmitter<any>;
     groups: Array<any>;
