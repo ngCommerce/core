221ba5f26beb32f3aa9a7fe2810f179cd6bbee4b
mob35 <toonelicious@gmail.com>
2017-10-24 16:20:20 +0700

    update

--- src/components/ion-form-credit/ion-form-credit.ts
+++ src/components/ion-form-credit/ion-form-credit.ts
@@ -65,18 +65,12 @@ export class IonFormCreditComponent {
   constructor(public parent: IonFormPaymentOptionComponent) {
     // console.log('Hello IonFormPaymentComponent Component');
   }
+
+
+
   formcredit(data) {
     if (data.creditno) {
-      let inputNum = data.creditno;
-      if (inputNum.length > 1) {
-        inputNum = inputNum.substr(inputNum.length - 1);
-      }
-      let num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-      if (num.indexOf(inputNum) !== -1) {
-        this.data.creditno = data.creditno;
-      } else {
-        this.data.creditno = data.creditno.substr(0, data.creditno.length - 1);
-      }
+      this.data.creditno = 'sssss';
     }
 
     if (data.creditcvc) {
