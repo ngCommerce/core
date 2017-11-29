64383729f6d706ca2fdbaae0c9227963ca2d27ea
mob35 <toonelicious@gmail.com>
2017-11-29 19:17:31 +0700

    update

--- src/components/ion-form-profile/ion-form-profile.ts
+++ src/components/ion-form-profile/ion-form-profile.ts
@@ -89,7 +89,7 @@ export class IonFormProfileComponent {
 
     logout() {
         window.localStorage.removeItem('token');
-        window.localStorage.removeItem('gCart');
+        // window.localStorage.removeItem('gCart');
         this.itemClicked.emit('logout');
     }
 
